bb247f4e2f6bc6a00902a3855048754bcc8ce79d
Serhi <serhi.melnik@gmail.com>
2015-12-01 01:12:31 +0200

    Fixed rotating when shpere instantiated and suggested as wall

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -23,16 +23,17 @@ public class AI : MonoBehaviour {
 			RaycastHit hit;
 			if (Physics.SphereCast (ray, 0.75f, out hit)) {
 				GameObject hitObject = hit.transform.gameObject;
-				if (hitObject.GetComponent<PlayerCharacter> ()) {
+				if (hitObject.GetComponent<PlayerCharacter>()) {
 					if (_fireball == null) {
 						_fireball = Instantiate(fireballPrefab) as GameObject;
 						_fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
 						_fireball.transform.rotation = transform.rotation;
 					}
-				} else if (hit.distance < obstacleRange) {
+				} 
+				else if (!hitObject.GetComponent<Fireball>() && hit.distance < obstacleRange) {
 					float angle = Random.Range(-110, 110);
 					transform.Rotate(0, angle, 0);
-				}	
+				}
 			}
 		}
 	}
